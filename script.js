4e9bcfa3b7a34ac9998c0a5eaaa4ccc640c0c78e
MORGANFREEMAAN <43350458+MORGANFREEMAAN@users.noreply.github.com>
2019-09-12 13:10:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//create global variable
+let sessions
+
+function loadPage() {
+fetch("sessions.json")
+    .then(response => response.json())
+    .then(json => {console.log(json)
+                  sessions = json
+                  createCards()})
+    .catch(err => console.error(err))
+//createCards()
+}
+function createCards() {
+let container = document.createElement('div')
+container.innerHTML = sessions.map(session =>
+`<div>
+<h3>${session.Title}</h3>
+<h3>Presenter${session['Presenter(s)'].split(' and ').length > 1 ? 's' : ''}: ${session['Presenter(s)']}</h3>
+<p>${session['Description']}</p>
+<button onclick="addToCart('${session['Title']}')">Sign Up</button>
+</div>`
+                                  ).join('')
+document.querySelector('article')
+    .append(container)
+}
+
+function addToCart(sessionName) {
+    let item = document.createElement('li')
+    item.innerHTML = sessionName
+
+    document.querySelector('ul#cart')
+        .append(item)
+}
